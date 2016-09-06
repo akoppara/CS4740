9c842ab058377b0be3a0323d0c6c2cef2487b935
ALEX <atk57@cornell.edu>
2016-09-06 14:48:19 -0400

    read previous commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 __author__ = 'Alex'
+import os
+
+def grab_files ():
+    path = "data_corrected\classification task"
+    for (path, dirs, files) in os.walk(path):
+        if len(files):
+            get_corpus(path, files)
+
+def get_corpus (path, files):
+    for file in files:
+        file_path = path + '\\' + file
+        open_file = open(file_path, 'r')
+        print (open_file.read())
+
+if __name__ == '__main__':
+    grab_files()
