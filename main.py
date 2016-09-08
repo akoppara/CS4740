bb9ac2295025faac6ef4ed6a27188f2f758b3246
JoeM557 <jam798@cornell.edu>
2016-09-08 12:33:18 -0400

    unigram probability calculations

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 __author__ = 'Alex'
 import os
+import re
 
-corpus_array = []
+corpus_dict = {}
+
+def get_corpus_name(path):
+    if 'train_docs' in path:
+        name = re.search(r'\\\S+\\', path).group(0)
+        #strip off slashes at both ends
+        name = name[1:len(name)-1]
+        return name
+    else:
+        return "test"        
 
 def grab_files ():
 
     path = "data_corrected\classification task"
     for (path, dirs, files) in os.walk(path):
         if len(files):
-            corpus_array.append(get_corpus(path, files))
-    print(corpus_array)
+            #Get the name of this corpus
+            name = get_corpus_name(path)
+            corpus_dict[name] = (get_corpus(path, files))
+    return corpus_dict
 
 def get_corpus (path, files):
     splitted_corpus = []
@@ -35,5 +47,40 @@ def get_corpus (path, files):
 
     return splitted_corpus
 
+#Get counts of individual tokens for unigram probabilities
+def count_tokens(corpus):
+    #corpus is an array of tokens from the data files
+    counts = {}
+    total = 0
+    for token in corpus:
+        total += 1
+        if token in counts:
+            counts[token] += 1
+        else:
+            counts[token] = 1
+    return (counts, total)
+
+#Calculates the unigram probabilities for a single corpus
+def calc_unigram_prob(counts, total):
+    probs = {}
+    for key in counts:
+        count = counts[key]
+        prob = float(count) / float(total)
+        probs[key] = prob
+    return probs
+
+def calc_all_corpora_unigram(corpora):
+    corpora_probs = {}
+    corpora_totals = {}
+    for key in corpora:
+        unigram_counts, total_tokens = count_tokens(corpora[key])
+        unigram_probs = calc_unigram_prob(unigram_counts, total_tokens)
+        corpora_probs[key] = unigram_probs
+        corpora_totals[key] = total_tokens
+
+    return (corpora_probs, corpora_totals)
+
 if __name__ == '__main__':
-    grab_files()
+    corpora = grab_files()
+    unigram_probs, corpora_totals = calc_all_corpora_unigram(corpora)
+    
